f52099937e9279406432ec075e63ba828b58725c
Jonghun Shin <jonghun.shin@linecorp.com>
2016-12-20 11:49:49 +0900

    handle exception

--- exams/management/commands/googletrends.py
+++ exams/management/commands/googletrends.py
@@ -10,13 +10,16 @@ class Command(BaseCommand):
     help = '단어 테이블에서 한 단어를 가져와 구글 트렌드 수치를 업데이트한다'
 
     @staticmethod
-    def __get_key(word : str):
+    def __get_key(word: str):
         key = re.sub(r'(\(.*\))', r'', word)
         key = re.sub(r'([^\u3040-\u309f\u30a0-\u30ff\u4e00-\u9fff]+)', r'', key)
         return key
 
     def handle(self, *args, **options):
-        word = Word.objects.filter(trends__isnull=True)[0]
+        try:
+            word = Word.objects.filter(trends__isnull=True)[0]
+        except IndexError:
+            return
 
         key = self.__get_key(word.word)
         trends = search_num(key)
@@ -24,4 +27,3 @@ class Command(BaseCommand):
         word.save()
 
         print(word.word + ' : ' + key + ' : ' + str(trends))
-
