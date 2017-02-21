0737b2cf4a77863da42c45d7bb121bcb6091e10f
Jonghun Shin <jonghun.shin@linecorp.com>
2017-02-21 17:24:56 +0900

    언락 시각을 새벽 4시로 변경하는 스크립트 추가

--- exams/tests.py
+++ exams/tests.py
@@ -2,9 +2,7 @@ from datetime import datetime, timezone
 
 from django.test import TestCase
 
-
-# Create your tests here.
-from exams import views
+from exams import utils
 
 
 class ExamsTestCase(TestCase):
@@ -15,27 +13,27 @@ class ExamsTestCase(TestCase):
         tz_local = datetime.now(timezone.utc).astimezone(tz=None).tzinfo
 
         a_datetime = datetime.strptime('2017-02-21 03:39:53.438223+0000', '%Y-%m-%d %H:%M:%S.%f%z')
-        result = views._normalize_date(a_datetime)
+        result = utils.normalize_date(a_datetime)
         expected = datetime(2017, 2, 21, 4, tzinfo=tz_local)
         self.assertEqual(result, expected)
 
         a_datetime = datetime.strptime('2017-02-20 02:00:00.000000+0900', '%Y-%m-%d %H:%M:%S.%f%z')
-        result = views._normalize_date(a_datetime)
+        result = utils.normalize_date(a_datetime)
         expected = datetime(2017, 2, 19, 4, tzinfo=tz_local)
         self.assertEqual(result, expected)
 
         a_datetime = datetime.strptime('2017-02-20 05:00:00.000000+0900', '%Y-%m-%d %H:%M:%S.%f%z')
-        result = views._normalize_date(a_datetime)
+        result = utils.normalize_date(a_datetime)
         expected = datetime(2017, 2, 20, 4, tzinfo=tz_local)
         self.assertEqual(result, expected)
 
         a_datetime = datetime.strptime('2017-02-20 00:00:00.000000+0900', '%Y-%m-%d %H:%M:%S.%f%z')
-        result = views._normalize_date(a_datetime)
+        result = utils.normalize_date(a_datetime)
         expected = datetime(2017, 2, 19, 4, tzinfo=tz_local)
         self.assertEqual(result, expected)
 
         a_datetime = datetime.strptime('2017-02-20 04:00:00.000000+0900', '%Y-%m-%d %H:%M:%S.%f%z')
-        result = views._normalize_date(a_datetime)
+        result = utils.normalize_date(a_datetime)
         expected = datetime(2017, 2, 20, 4, tzinfo=tz_local)
         self.assertEqual(result, expected)
 
