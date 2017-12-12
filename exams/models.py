ee5a12940052f25965f7f7b5cc3a45568e7d93ed
Jonghun Shin <jonghun.shin@linecorp.com>
2017-12-12 17:37:14 +0900

    Upgrading to Django 2.0

--- exams/models.py
+++ exams/models.py
@@ -1,7 +1,7 @@
 import re
 
 from django.contrib.auth.models import User
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.db import models
 from django.utils import timezone
 
